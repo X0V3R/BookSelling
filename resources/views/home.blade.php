457580afd94f917d4b292f0ee8ff332e9541d01e
X0V3R <heaven7hack@gmail.com>
2019-12-15 15:27:31 +0700

    update text-truncate & tooltip

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('search')
-                @include('book.search')
+    @include('book.search')
 @endsection
 
 @section('content')
@@ -14,14 +14,19 @@
 
         {{-- EXPLAIN: MENU SIDE BAR
              --}}
+             {{-- TODO: thử với item-wrapper --}}
         @include('category.show')
         @foreach ($data as $book)
         <div class="col-sm-2 col-md-3">
                 {{-- <div class="products"> --}}
-                    <div class="card" style="width: 250px">
+                    <div class="card" style="max-width: 250px;">
                         <img src="{{asset($book->image)}}" class="card-img-top" style="height: 250px; width: 250px;" alt="">
                         <div class="card-body">
-                            <div class="card-title">{{$book->name}}</div>
+                            <div class="card-title d-inline-block" tabindex="0" data-toggle="tooltip" title="{{$book->name}}">
+                                <span class="text-truncate">
+                                    {{$book->name}}
+                                </span>
+                            </div>
                             <p class="card-text">{{$book->detail}}</p>
                             <p class="card-text">{{$book->cost}}</p>
                             {{-- <p class="card-text">{{$book->id_category}}</p> --}}
